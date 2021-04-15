a7fe1178fb8dd591cbc3b7f19fa7339ecb5e156b
whacked <whacked@users.noreply.github.com>
2021-04-15 15:29:39 -0700

    add pglast + jsonnet2sql script + handy alias

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
 with import <nixpkgs> {};
 
 let
-  jsonschema2db = import ./jsonschema2db.nix;
+  python38Packages_jsonschema2db = import ./jsonschema2db.nix;
+  python38Packages_pglast = python38.pkgs.buildPythonPackage rec {
+    pname = "pglast";
+    version = "1.17";
+    src = python38.pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "1ygmwf39wzx6qzm06zzf8xv6mnq45pvg52mpbl5glb7pln6b6y99";
+    };
+    checkInputs = [ ];
+  };
 in stdenv.mkDerivation rec {
   name = "BorderScreama";
   env = buildEnv {
@@ -9,17 +18,24 @@ in stdenv.mkDerivation rec {
     paths = buildInputs;
   };
   buildInputs = [
+    jq
+    jsonnet
+    nodejs
     python38Packages.coloredlogs
+    python38Packages.jsonnet
     python38Packages.jsonschema
     python38Packages.pyaml
     python38Packages.pydantic
-    nodejs
+    python38Packages_pglast
+    python38Packages_jsonschema2db
     watchexec
-    jsonschema2db
   ];
 
   shellHook = ''
     export PATH=$PATH:$(npm bin)
     unset name
+
+    alias jsonnet2sql='python jsonnet2sql.py'
+    cat ${__curPos.file} | grep '^[[:space:]]*\(function\|alias\).*'
   '';
 }
---
+++ jsonnet2sql.py
@@ -0,0 +1,48 @@
+import sys
+import json
+import _jsonnet as jsonnet
+from jsonschema2db import JSONSchemaToPostgres
+
+
+input_file_path = sys.argv[-1]
+assert input_file_path.endswith('jsonnet')
+
+json_string = jsonnet.evaluate_file(input_file_path)
+schema = json.loads(json_string)
+
+
+class DummyConnection:
+    sql_history = []
+
+    @classmethod
+    def get_statements(cls):
+        return cls.sql_history
+
+    def cursor(connection):
+        class Lifecycle:
+            class DummyCursor:
+                def execute(self, query, args):
+                    connection.sql_history.append((query, args))
+            
+            def __enter__(self):
+                return self.DummyCursor()
+            def __exit__(self, *args):
+                pass
+
+        return Lifecycle()
+
+
+JSONSchemaToPostgres(
+    schema,
+    postgres_schema='schm',
+    abbreviations={
+        # source column name: target column name
+        'AbbreviateThisReallyLongColumn': 'AbbTRLC'
+    },
+).create_tables(DummyConnection())
+
+from pglast import prettify
+for (query, args) in DummyConnection.get_statements():
+    if query.startswith('create'):
+        sql_pretty = prettify(query)
+        sys.stdout.write(f'{sql_pretty}\n')
