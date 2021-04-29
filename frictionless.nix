ac9b709eda28187906c66d1c9679f281f619d471
whacked <whacked@users.noreply.github.com>
2021-04-29 13:25:44 -0700

    add handy function alias as usage reminder

--- frictionless.nix
+++ frictionless.nix
@@ -20,6 +20,10 @@ pkgs.mkShell {
         pip install frictionless frictionless[sql] tableschema tableschema-sql psycopg2 pydantic sqlparse ddlparse
     }
 
+    export PYTHONPATH=$PWD:$PYTHONPATH
+    alias test-frictionless='python tests/test_frictionless_capture.py'
     ensure-venv initialize-venv
+
+    echo-shortcuts ${__curPos.file}
   '';
 }
