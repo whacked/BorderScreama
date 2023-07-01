4ec01912ddc8294e2749f0ffa25b513b3cfa39f3
whacked-xps <whacked@users.noreply.github.com>
2023-07-01 20:34:34 +0800

    add spelled-out deps for datamodel-code-generator

--- flake.nix
+++ flake.nix
@@ -17,6 +17,59 @@
       pkgs = nixpkgs.legacyPackages.${system};
       whacked-helpers = import (whacked-setup + /nix/flake-helpers.nix) { inherit pkgs; };
       python3Packages_jsonschema2db = import ./jsonschema2db.nix { inherit pkgs; };
+      python3Packages_datamodel_codegen = pkgs.python3.pkgs.buildPythonPackage rec {
+        pname = "datamodel_code_generator";
+        format = "pyproject";
+        version = "0.20.0";
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-hNx9auZMpng0tBTBB63zUQ9GpHSsIUZ6aDrKCkqPCAY=";
+        };
+        propagatedBuildInputs = [
+          pkgs.python3Packages.setuptools
+          pkgs.python3Packages.poetry-core
+          pkgs.python3Packages.jinja2
+          pkgs.python3Packages.toml
+          pkgs.python3Packages.prance
+          pkgs.python3Packages.jsonschema
+          pkgs.python3Packages.argcomplete
+          pkgs.python3Packages.isort
+          pkgs.python3Packages.pysnooper
+          pkgs.python3Packages.black
+          pkgs.python3Packages.email-validator
+          (pkgs.python3Packages.openapi-spec-validator.overridePythonAttrs(old: rec {
+            version = "0.5.2";
+            format = "pyproject";
+            src =  pkgs.fetchPypi {
+              pname = "openapi_spec_validator";
+              inherit version;
+              sha256 = "sha256-6+1/HFZ3gIWUAq1ksSjhf1GdFfYF8bQdHppKehaQvgc=";
+            };
+            doCheck = false;
+          }))
+          (pkgs.python3Packages.inflect.overridePythonAttrs(old: rec {
+            version = "5.6.2";
+            src =  pkgs.fetchPypi {
+              pname = "inflect";
+              inherit version;
+              sha256 = "sha256-qtx+1zko9eAUEpeUu6wDBYzKNdCpc6X8TrRcf6JgBfk=";
+            };
+          }))
+          (pkgs.python3.pkgs.buildPythonPackage rec {
+            pname = "genson";
+            version = "1.2.2";
+            format = "pyproject";
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-jK9pqhCveu4OGhNR0dBoAfRpbgBfBs7e9DhjU4Q0ahY=";
+            };
+            buildInputs = [
+              pkgs.python3Packages.jsonschema_3
+              pkgs.python3Packages.setuptools
+            ];
+          })
+        ];
+      };
       python3Packages_pglast = pkgs.python3.pkgs.buildPythonPackage rec {
         pname = "pglast";
         version = "1.17";
@@ -41,14 +94,16 @@
           pkgs.python3Packages.icecream
           pkgs.python3Packages.ipython
           pkgs.python3Packages.jsonnet
-          pkgs.python3Packages.jsonschema
+          pkgs.python3Packages.jsonschema_3
           pkgs.python3Packages.pyaml
           pkgs.python3Packages.pydantic
           pkgs.python3Packages.psycopg2
           pkgs.python3Packages.sqlalchemy
           python3Packages_pglast
+          python3Packages_datamodel_codegen
           python3Packages_jsonschema2db
           pkgs.watchexec
+
         ];
 
         shellHook = ''
